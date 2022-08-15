72c2d8019481b4c3e2dbd6f92e7d7a7e75ea73f3
Bartłomiej Winiarski <32848266+returnv01d@users.noreply.github.com>
2022-08-16 00:45:51 +0200

    Create TALKPAIRS.cpp

---
+++ TALKPAIRS.cpp
@@ -0,0 +1,54 @@
+// https://www.spoj.com/problems/TALKPAIRS/
+
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    unsigned long * tab = new unsigned long [n];
+	for(int i = 0;i < n;i++) cin >> tab[i];
+
+    int current, next, highest, second_highest;
+    int sum = 0;
+
+    for(int i = 0; i < n; i++){
+        current = tab[i];
+        highest = 0;
+        second_highest = 0;
+
+        for(int a = i + 1; a < n; a++){
+            next = tab[a];
+
+			// Case when first next number is bigger, we dont have max yet
+			if(current <= next){
+              sum++;
+              break;
+            }
+
+            if(current <= highest){
+              sum++;
+              break;
+            }
+            if(next <= highest || next <= second_highest) continue;
+
+            sum++;
+
+			// Update highest and second highest numbers
+            if(next > highest) {
+                second_highest = highest;
+                highest = next;
+            }
+            if(next >= second_highest && next < highest ) second_highest = next;
+        }
+    }
+
+    cout << sum;
+
+	delete tab;
+
+	return 0;
+}
